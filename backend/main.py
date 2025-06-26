2bf4b5b14fb4814f93490a88cd2273d4d0669ab0
NurStarUz <nurstarpul@gmail.com>
2025-06-26 12:00:57 +0500

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -104,4 +104,4 @@ def get_reason_description(reason):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)
