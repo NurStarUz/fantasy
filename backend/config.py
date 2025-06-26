44896d94b9b5ef72c893ae30fc10e8e811b0c290
NurStarUz <nurstarpul@gmail.com>
2025-06-26 11:45:57 +0500

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost:5432/fantasy_football")
-    FOOTBALL_API_KEY = os.getenv("FOOTBALL_API_KEY")
-    TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://nurstar:9zM1pSmDMwO3fKovJ2hQlyQbnxZSVHDW@dpg-d1eegdp5pdvs73bvi4jg-a/fantasydb_tise")
+    FOOTBALL_API_KEY = os.getenv("bbb143c3e3834132934ccf14fa9343ea")
+    TELEGRAM_BOT_TOKEN = os.getenv("7810185081:AAHX-vIcHd8_2kwlEUoq6_BBpeQ5cTLWMGM")
     TZ = os.getenv("TZ", "Asia/Tashkent")
