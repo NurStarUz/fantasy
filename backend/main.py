cf9961bc0f60d779e81a16e548528dfa8a9441fb
NurStarUz <nurstarpul@gmail.com>
2025-06-27 11:06:39 +0500

    Create main.py

---
+++ backend/main.py
@@ -0,0 +1,26 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from routers import users, teams, transfers, points, updater
+
+app = FastAPI()
+
+origins = ["*"]  # Frontend CORS uchun
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routerlar
+app.include_router(users.router)
+app.include_router(teams.router)
+app.include_router(transfers.router)
+app.include_router(points.router)
+app.include_router(updater.router)
+
+@app.get("/")
+def root():
+    return {"msg": "Fantasy Football API is running"}
