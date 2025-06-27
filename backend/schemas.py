83ad7338a0b5e3b588358dc14f055bc7d020494a
NurStarUz <nurstarpul@gmail.com>
2025-06-27 11:07:31 +0500

    Create schemas.py

---
+++ backend/schemas.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import date
+
+class UserBase(BaseModel):
+    telegram_id: str
+    full_name: Optional[str]
+    avatar: Optional[str]
+
+class TeamBase(BaseModel):
+    player_ids: List[int]
+
+class TransferBase(BaseModel):
+    player_out: int
+    player_in: int
+
+class PlayerPoint(BaseModel):
+    player_id: int
+    match_date: date
+    goals: int
+    assists: int
+    clean_sheet: int
+    yellow_cards: int
+    red_cards: int
+    total_points: int
